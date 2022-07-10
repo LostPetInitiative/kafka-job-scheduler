a30f342096d120cf5428e8b2599e4ed6c72fcd01
Dmitry A. Grechka <dmitry@grechka.family>
2022-07-10 18:51:42 +0000

    updating depts

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -7,7 +7,7 @@ class TestSum(unittest.TestCase):
         producer = JobQueueProducer('localhost:9092', 'test_topic', 'test_producer', replication_factor=1)
         consumer = JobQueueWorker('test_consumer',kafkaBootstrapUrl='localhost:9092',topicName='test_topic',appName='test_consumer')
         producer.Enqueue('test_job', {'test': 'test'})
-        job = consumer.TryGetNextJob()
+        job = consumer.TryGetNextJob(5000)
         consumer.Commit()
         self.assertDictEqual(job, {'test': 'test'}, "Should be {'test': 'test'}")
 
